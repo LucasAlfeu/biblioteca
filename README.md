8380dc968fce8dc180feff73c2346951478b8622
LucasAlfeu <lucas.a1feu19@gmail.com>
2024-06-18 10:59:44 -0300

    Arrumando o GitHub

--- README.md
+++
@@ -1,2 +0,0 @@
-# biblioteca
-Repositório para o trabalho de faculdade em Orientação a Objetos em Java da disciplina Linguagem de Programação 3
---
+++ biblioteca/src/main/java/br/biblioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.biblioteca;
+
+/**
+ *
+ * @author a1feu
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
