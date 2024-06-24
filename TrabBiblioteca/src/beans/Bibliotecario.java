010941a1a74cb3d1b98b625925017c4e9f5b3040
r4fael1472 <rafafajorge@gmail.com>
2024-06-24 09:11:37 -0300

    Faltou o Bibliotecário

---
+++ TrabBiblioteca/src/beans/Bibliotecario.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package beans;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Bibliotecario extends Usuario{
+    private String idBibliotecario;
+    
+    public Bibliotecario(){
+        
+    }
+
+    public Bibliotecario(String nome, String email, String login, String password) {
+        super(nome, email, login, password);
+    }
+    
+    
+    
+    
+}
